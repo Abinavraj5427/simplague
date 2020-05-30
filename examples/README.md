eb6dc99dfa1c51636425474c0b2455e9a673fc8a
Abinavraj5427 <abinavraj10@gmail.com>
2020-05-30 00:03:18 -0500

    Added front end UI for variable manipulation

--- examples/README.md
+++
@@ -1,2 +0,0 @@
-# simplague
-A simulation of the entire world with manipulation factors to advertise products to prevent infection growth rate of diseases.
--- examples/index.js
+++ examples/index.js
@@ -15,6 +15,37 @@ var mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
+var population_slider = document.getElementById("population");
+var population_label = document.getElementById("population_label");
+population_label.innerHTML = "Population: "+population_slider.value; // Display the default slider value
+population_slider.oninput = function() {population_label.innerHTML = "Population: "+this.value;}// Update the current slider value (each time you drag the slider handle)
+
+var social_distance_slider = document.getElementById("social_distance");
+var social_distance_label = document.getElementById("social_distance_label");
+social_distance_label.innerHTML = "Social Distance: "+social_distance_slider.value; // Display the default slider value
+social_distance_slider.oninput = function() {social_distance_label.innerHTML = "Social Distance: "+this.value;}// Update the current slider value (each time you drag the slider handle)
+
+var social_distance_rate_slider = document.getElementById("social_distance_rate");
+var social_distance_rate_label = document.getElementById("social_distance_rate_label");
+social_distance_rate_label.innerHTML = "Social Distance Participation Rate: "+social_distance_slider.value+"%"; // Display the default slider value
+social_distance_rate_slider.oninput = function() {social_distance_rate_label.innerHTML = "Social Distance Participation Rate: "+this.value+"%";}// Update the current slider value (each time you drag the slider handle)
+
+var infection_radius_slider = document.getElementById("infection_radius");
+var infection_radius_label = document.getElementById("infection_radius_label");
+infection_radius_label.innerHTML = "Infection Radius: "+infection_radius_slider.value; // Display the default slider value
+infection_radius_slider.oninput = function() {infection_radius_label.innerHTML = "Infection Radius: "+this.value;}// Update the current slider value (each time you drag the slider handle)
+
+var infection_prob_slider = document.getElementById("infection_prob");
+var infection_prob_label = document.getElementById("infection_prob_label");
+infection_prob_label.innerHTML = "Infection Probability: "+infection_prob_slider.value+"%"; // Display the default slider value
+infection_prob_slider.oninput = function() {infection_prob_label.innerHTML = "Infection Probability: "+this.value+"%";}// Update the current slider value (each time you drag the slider handle)
+
+var transportation_rate_slider = document.getElementById("transportation_rate");
+var transportation_rate_label = document.getElementById("transportation_rate_label");
+transportation_rate_label.innerHTML = "Transportation Rate: "+transportation_rate.value+"%"; // Display the default slider value
+transportation_rate_slider.oninput = function() {transportation_rate_label.innerHTML = "Transportation Rate: "+this.value+"%";}// Update the current slider value (each time you drag the slider handle)
+
+
 // immediately use the texture for material creation
 var material = new THREE.ShaderMaterial({
 
